6f63853a1b2c65403efc3363ed3a5a2ef8153292
harugramming <haruuchi.pool.0219@gmail.com>
2021-11-07 10:42:14 +0900

    プロフィール変更画面を作成しました。

--- laravel/resources/views/users/edit.blade.php
+++ laravel/resources/views/users/edit.blade.php
@@ -1,1 +1,21 @@
-edit
+@extends('app')
+
+@section('title', $user->name)
+
+@section('content')
+  @include('nav')
+  <div class="container">
+    @include('users.user')
+    <div class="container">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+        <div class="md-form">
+            <label for="nickname">ユーザー名（表示名）</label>
+            <input class="form-control" type="text" id="nickname" name="nickname" required value="{{ $user->nickname}}">
+        </div>
+        <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー名変更</button>
+        </form>
+    </div>
+      <!-- /.container -->
+  </div>
+@endsection

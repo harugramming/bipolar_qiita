9d1abf0729b2930bb6cb80f12132129552e7498b
harugramming <haruuchi.pool.0219@gmail.com>
2021-11-29 13:12:14 +0900

    会員登録画面、ログイン画面のレイアウトを修正しました。

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -3,26 +3,31 @@
 @section('title', 'ログイン')
 
 @section('content')
-  <div class="container">
+  <div class="register-container" style="background-color: #fff;">
     <div class="row">
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">bipolarTips</a></h1>
-        <div class="card mt-3">
+        <h1 class="text-center"><a class="text-dark" href="/"></a></h1>
+        <div class="">
           <div class="card-body text-center">
-            <h2 class="h3 card-title text-center mt-2">ログイン</h2>
-
+            <h1>bipolarTips</h1>
+            <p style="margin-top:10px;">ログインをして利用を開始しましょう。</p>
+            <hr>
+            <form class="btn btn-twitter-inverse btn-block btn-lg" method="get" action="auth/login/twitter" style="padding:0;"><button type="submit" style="color:#fff; width:100%; padding: 14px;
+                font-size: 18px;"><i class="fab fa-twitter" style="    margin-right: 4px;"></i></i>Twitterアカウントで登録</button><input type="hidden" name="authenticity_token" value="sm5m2kAP4Q99QQu9ljhuL3Ifpu9EtVNhs83Gno153GrMIDWRjxaYXKYlz4m3B+yyfSI7TW0CoYsPWXaLwZ9Lqg=="></form>
+                <a href="/login/google" class="btn btn-secondary btn-lg btn-block btn-lg waves-effect waves-light" style="background-color: #c6594b !important;
+                border-color: #a94335; margin-top: 10px;"role="button"><i class="fab fa-google" style="margin-right: 4px;"></i>Googleアカウントで登録<input type="hidden" name="authenticity_token" value="MgbnjeB9Q8LwLXbwBje7QWrl9vZ64vd2oJ5k2pJVWwhMSLTGL2Q6kStJssQnCDncZdhrVFNVBZwcCtTP3rPMyA=="></a>
             @include('error_card_list')
 
             <div class="card-text">
               <form method="POST" action="{{ route('login') }}">
                 @csrf
 
-                <div class="md-form">
+                <div class="">
                   <label for="email">メールアドレス</label>
                   <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
                 </div>
 
-                <div class="md-form">
+                <div class="">
                   <label for="password">パスワード</label>
                   <input class="form-control" type="password" id="password" name="password" required>
                 </div>
@@ -36,7 +41,7 @@
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
               </form>
-              <form class="btn btn-twitter-inverse btn-block btn-lg" method="get" action="auth/login/twitter"><button type="submit"><i class="fab fa-twitter"></i></i>Twitterアカウントで登録</button><input type="hidden" name="authenticity_token" value="sm5m2kAP4Q99QQu9ljhuL3Ifpu9EtVNhs83Gno153GrMIDWRjxaYXKYlz4m3B+yyfSI7TW0CoYsPWXaLwZ9Lqg=="></form>
+
               <div class="mt-0">
                 <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
               </div>
--- laravel/resources/views/auth/register.blade.php
+++ laravel/resources/views/auth/register.blade.php
@@ -3,37 +3,41 @@
 @section('title', 'ユーザー登録')
 
 @section('content')
-  <div class="container">
+  <div class="register-container" style="background-color: #fff;">
     <div class="row">
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">bipolarTips</a></h1>
-        <div class="card mt-3">
+        <div class="">
           <div class="card-body text-center">
-            <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
-
+              <h1>bipolarTips</h1>
+              <p style="margin-top:10px;">新規登録(無料)をして利用を開始しましょう。</p>
+              <hr>
+              <form class="btn btn-twitter-inverse btn-block btn-lg" method="get" action="auth/login/twitter" style="padding:0;"><button type="submit" style="color:#fff; width:100%; padding: 14px;
+                font-size: 18px;"><i class="fab fa-twitter" style="    margin-right: 4px;"></i></i>Twitterアカウントで登録</button><input type="hidden" name="authenticity_token" value="sm5m2kAP4Q99QQu9ljhuL3Ifpu9EtVNhs83Gno153GrMIDWRjxaYXKYlz4m3B+yyfSI7TW0CoYsPWXaLwZ9Lqg=="></form>
+                <a href="/login/google" class="btn btn-secondary btn-lg btn-block btn-lg waves-effect waves-light" style="background-color: #c6594b !important;
+                border-color: #a94335; margin-top: 10px;"role="button"><i class="fab fa-google" style="margin-right: 4px;"></i>Googleアカウントで登録<input type="hidden" name="authenticity_token" value="MgbnjeB9Q8LwLXbwBje7QWrl9vZ64vd2oJ5k2pJVWwhMSLTGL2Q6kStJssQnCDncZdhrVFNVBZwcCtTP3rPMyA=="></a>
             @include('error_card_list')
 
             <div class="card-text">
               <form method="POST" action="{{ route('register') }}">
                 @csrf
-                <div class="md-form">
+                <div class="">
                   <label for="name">ユーザーID</label>
                   <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
                   <small>英数字3〜16文字(登録後の変更はできません)</small>
                 </div>
-                <div class="md-form">
+                <div class="">
                   <label for="nickname">ユーザー名（表示名）</label>
                   <input class="form-control" type="text" id="nickname" name="nickname" required value="{{ old('nickname') }}">
                 </div>
-                <div class="md-form">
+                <div class="">
                   <label for="email">メールアドレス</label>
                   <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}" >
                 </div>
-                <div class="md-form">
+                <div class="">
                   <label for="password">パスワード</label>
                   <input class="form-control" type="password" id="password" name="password" required>
                 </div>
-                <div class="md-form">
+                <div class="">
                   <label for="password_confirmation">パスワード(確認)</label>
                   <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
                 </div>
@@ -41,11 +45,6 @@
                 <div class="form-group"><input id="privacy" required="required" style="margin-right: 4px;" type="checkbox"><label for="privacy"><a href="/privacypolicy" target="_blank">プライバシーポリシー</a>に同意する</label></div>
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
               </form>
-              <form class="btn btn-twitter-inverse btn-block btn-lg" method="get" action="auth/login/twitter"><button type="submit"><i class="fab fa-twitter"></i></i>Twitterアカウントで登録</button><input type="hidden" name="authenticity_token" value="sm5m2kAP4Q99QQu9ljhuL3Ifpu9EtVNhs83Gno153GrMIDWRjxaYXKYlz4m3B+yyfSI7TW0CoYsPWXaLwZ9Lqg=="></form>
-              <a href="/login/google" class="btn btn-secondary" role="button">
-                Google Login
-              </a>
-
               <div class="mt-0">
                 <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
               </div>

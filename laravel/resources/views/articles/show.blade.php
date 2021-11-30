518892927ef6cb354027cedef023e178437893ca
harugramming <haruuchi.pool.0219@gmail.com>
2021-11-30 10:22:14 +0900

    コメント機能のプロフィール画像の大きさを修正しました。

--- laravel/resources/views/articles/show.blade.php
+++ laravel/resources/views/articles/show.blade.php
@@ -57,11 +57,11 @@
     <div class="comments">
         <div class="comment-container-profiles">
             @if(strpos($comments[$i]->profile_image,'http:') !== false)
-            <img src="{{ $comments[$i]->profile_image }}">
+            <img src="{{ $comments[$i]->profile_image }}" style="width:70px;">
             @else
-            <img src="{{ asset('storage/profiles/'.$comments[$i]->profile_image) }}" alt="プロフィール画像">
+            <img src="{{ asset('storage/profiles/'.$comments[$i]->profile_image) }}" alt="プロフィール画像" style="width:70px;">
             @endif
-            <a href="{{ route('users.show', ['name' => $comments[$i]->name]) }}" class="text-dark">
+            <a href="{{ route('users.show', ['name' => $comments[$i]->name]) }}" class="text-dark" >
             {{ $comments[$i]->nickname }}
             @
             {{ $comments[$i]->name }}

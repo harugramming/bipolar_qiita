bd5ab83ea8b97d412873f32e827dc66c867c4f43
harugramming <haruuchi.pool.0219@gmail.com>
2021-11-30 10:55:32 +0900

    記事投稿画面のレイアウトを修正しました。

--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -7,6 +7,9 @@
   <label></label>
   <input name="body" hidden style="height:1px;margin:0;">
 </div>
+<p style="margin: 0;
+color: #888;
+font-size: 16px;">本文</p>
 <div id="editor" style="height:300px;">
     {!! $article->body ?? old('body') !!}
 </div>
